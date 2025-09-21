c8596b34651ce2da4e12e5698c8df9c696579fe3
Danny Sheahan <dannysheahan@gmail.com>
2025-09-21 12:42:09 +0100

    Ensure lowercase app/toolbox path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+﻿// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOW-FROM https://www.delve-research.com" },
+          { key: "Content-Security-Policy", value: "frame-ancestors https://www.delve-research.com;" },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
+import { SpeedInsights } from "@vercel/speed-insights/next"
